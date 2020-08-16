0568f1be8ce663b69b98256609472709e58aa888
Balazs Vecsey <387778+Balage@users.noreply.github.com>
2020-08-16 15:58:56 +0200

    Removed program switch, and kept only the lock/sleep mode

--- Source Code/BigRedButton/BigRedButton.ino
+++ Source Code/BigRedButton/BigRedButton.ino
@@ -32,8 +32,6 @@
 // The button and the switches must be connected to
 // analog pins for the Schmitt trigger to work
 #define IO_BUTTON A0
-#define IO_SWITCH_1 A2
-#define IO_SWITCH_2 A1
 
 // The LED must be on a PWM pin, on the
 // Arduino Leonardo (MEGA32U4) these are: 3, 5, 6, 9, 10
@@ -54,39 +52,21 @@
 //
 // BUTTON BEHAVIOR
 //
-void buttonEvent(int selectedProgram,
+void buttonEvent(
   bool pressed, bool released, bool shortReleased,
   bool singleClick, bool doubleClick, bool longPressed, bool longDoubleClick)
 {
   // All inputs are events and fired only once.
-  // They are paired into 3 groups, and each program should only rely on one group.
+  // They are paired into 3 groups, and the program should only rely on one group.
   // Group #1: [ pressed, released ]
   // Group #2: [ shortReleased, longPressed ]
   // Group #3: [ singleClick, doubleClick, longPressed, longDoubleClick ]
   
-  switch (selectedProgram)
-  {
-    case 0:
-      if (pressed) Keyboard.HoldKey(KEY_ENTER);
-      if (released) Keyboard.ReleaseKey();
-      break;
-      
-    case 1:
-      if (pressed) Keyboard.HoldKey(KEY_SPACE);
-      if (released) Keyboard.ReleaseKey();
-      break;
-      
-    case 2:
-      if (singleClick) Keyboard.PressKey(KEY_F13);
-      if (doubleClick) Keyboard.PressKey(KEY_F14);
-      if (longPressed) Keyboard.PressKey(KEY_F15);
-      if (longDoubleClick) Keyboard.PressKey(KEY_F16);
-      break;
-      
-    case 3:
-      if (shortReleased) Keyboard.PressKey(KEY_L, MOD_LEFT_GUI);
-      if (longPressed) Keyboard.PressKeyPage1(KEY1_SYSTEM_SLEEP);
-      break;
+  if (shortReleased) {
+    Keyboard.PressKey(KEY_L, MOD_LEFT_GUI);
+  }
+  if (longPressed) {
+    Keyboard.PressKeyPage1(KEY1_SYSTEM_SLEEP);
   }
 }
 
@@ -106,22 +86,14 @@ bool _nextReleaseIsDoubleClick = false;
 bool _longPressFired = false;
 bool _buttonLastState = false;
 float _ledBrightness = 0.0f;
-int _lastProgram;
-bool _progSwitchState1 = false;
-bool _progSwitchState2 = false;
 
 void setup() {
   pinMode(IO_BUTTON, INPUT);
-  pinMode(IO_SWITCH_1, INPUT);
-  pinMode(IO_SWITCH_2, INPUT);
   pinMode(IO_LIGHT, OUTPUT);
 
   // Set default values
   digitalWrite(IO_LIGHT, HIGH);
-  _lastProgram = readProgramSwitch();
   _lastTimestamp = millis();
-
-  Serial.begin(9600);
 }
 
 bool schmittRead(int pin, bool lastState) {
@@ -134,14 +106,6 @@ bool schmittRead(int pin, bool lastState) {
   return lastState;
 }
 
-int readProgramSwitch()
-{
-  _progSwitchState1 = schmittRead(IO_SWITCH_1, _progSwitchState1);
-  _progSwitchState2 = schmittRead(IO_SWITCH_2, _progSwitchState2);
-  
-  return (_progSwitchState1 ? 1 : 0) | (_progSwitchState2 ? 2 : 0);
-}
-
 void loop() {
   unsigned long timestamp = millis();
   unsigned long delta_i = timestamp - _lastTimestamp;
@@ -151,24 +115,12 @@ void loop() {
     _lastTimestamp = timestamp;
     float delta = delta_i / 1000.0f;
 
-    int program = getAndUpdateProgram();
-    updateButton(program);
+    updateButton();
     updateLED(delta);
   }
 }
 
-int getAndUpdateProgram() {
-  int program = readProgramSwitch();
-  if (program != _lastProgram)
-  {
-    // Avoid any keys getting stuck while changing program
-    Keyboard.ReleaseKey();
-    _lastProgram = program;
-  }
-  return program;
-}
-
-void updateButton(int program)
+void updateButton()
 {
   unsigned long timestamp = millis();
   bool buttonState = schmittRead(IO_BUTTON, _buttonLastState);
@@ -237,9 +189,10 @@ void updateButton(int program)
     _doubleClickInProgress = false;
   }
   
-  buttonEvent(program,
+  buttonEvent(
     buttonPressed, buttonReleased, buttonShortReleased,
-    buttonSingleClick, buttonDoubleClick, buttonLongPressed, buttonLongDoubleClick);
+    buttonSingleClick, buttonDoubleClick, buttonLongPressed, buttonLongDoubleClick
+  );
   
   _buttonLastState = buttonState;
 }
